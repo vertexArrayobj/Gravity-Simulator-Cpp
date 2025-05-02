49f13555fc096f84bce99110e77fe08732d38c85
vertexArrayobj <mhrohanbd@gmail.com>
2025-05-03 01:48:02 +0600

    Linux compliant

--- src/GLErrorManager.h
+++ src/GLErrorManager.h
@@ -2,16 +2,26 @@
 
 #include <GL/glew.h>
 
-#define ASSERT(x) if (!(x)) __debugbreak();
+#ifdef _MSC_VER
+#define DEBUG_BREAK() __debugbreak()
+#else
+#include <signal.h>
+#define DEBUG_BREAK() raise(SIGTRAP)
+#endif
+
+#define ASSERT(x) \
+	if (!(x))     \
+		DEBUG_BREAK();
 
 #ifdef _DEBUG
-#define GLCall(x) GLClearError();\
-		x;\
-		ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+#define GLCall(x)   \
+	GLClearError(); \
+	x;              \
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 #else
 #define GLCall(x) x
 #endif
 
 void GLClearError();
 
-bool GLLogCall(const char* func, const char* file, int line);
+bool GLLogCall(const char *func, const char *file, int line);
